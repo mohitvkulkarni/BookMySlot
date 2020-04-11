9336403fd7c9a3e9d375ea2d819f2b5bd8ff0512
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2020-04-11 18:56:57 +0530

    Styling and Images

--- src/Components/CustomerTimeSlot.js
+++ src/Components/CustomerTimeSlot.js
@@ -3,13 +3,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import Button from "../Components/Button";
 import { Link } from "react-router-dom";
 
-
 class CustomerTimeSlot extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row align-items-center">
-          <div className="col-12 slotContainerDiv row">
+        <div className="row" style={{ justifyContent: "center" }}>
+          <div className="col-11 slotContainerDiv row">
             <div className="col-7">
               <h7 className="slotListHeader">Time: {this.props.slotTime}</h7>
               <br></br>
@@ -18,22 +17,20 @@ class CustomerTimeSlot extends Component {
               </h8>
             </div>
             <div className="col-5">
-              <div className="col-12 centerContent">
-                <Link to = {"/BookingSuccessful"}>
+              <Link to={"/BookingSuccessful"}>
                 <button
                   type="button"
                   style={{
-                    backgroundColor: "#5669F0",
-                    borderColor: "#5669F0",
-                    height: 50,
+                    backgroundColor: "#414157",
+                    borderColor: "#414157",
+                    padding: 10,
                     color: "#fff",
                   }}
                   className="btn btn-primary"
                 >
                   BOOK SLOT
                 </button>
-                </Link>
-              </div>
+              </Link>
             </div>
           </div>
         </div>
